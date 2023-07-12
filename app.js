dff043cfa90d4be7cad123eba7e0aa9dd35223a8
hemanthchinnam <urs838hemanthch@gmail.com>
2023-07-12 10:21:30 +0530

    full stage pipeline Test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello Welcome to the project
-all the stages of project are done!'));
+all the stages for the project are done!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
