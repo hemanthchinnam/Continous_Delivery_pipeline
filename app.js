b8e4710c87b123b0046c5d743ec809c8ca659c31
hemanthchinnam <urs838hemanthch@gmail.com>
2023-07-12 10:14:29 +0530

    full pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Welcome to the project'));
+app.get('/', (req, res) => res.send('Hello Welcome to the project
+all the stages of project are done!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
